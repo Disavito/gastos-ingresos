b59681b578e458c952d8de54c9b95eccf4e4a125
diego <dsvt98@gmail.com>
2025-08-27 14:29:42 -0500

    Removed unused folders

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [
     react({
-      jsxRuntime: 'automatic' // CRÍTICO: Hace explícito el JSX Runtime automático para Vite
+      jsxRuntime: 'automatic'
     })
   ],
   optimizeDeps: {
